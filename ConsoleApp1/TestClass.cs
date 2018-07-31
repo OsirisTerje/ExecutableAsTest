875909301b0491844cc1f80720bba60e3ddb65bf
Terje Sandstrom <terje@hermit.no>
2018-07-31 21:07:17 +0200

    Seems to work in VS at least

---
+++ ConsoleApp1/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    public class TestClass
+    {
+        [Test]
+        public void ThatsIt()
+        {
+            Assert.That(true);
+        }
+    }
+}
